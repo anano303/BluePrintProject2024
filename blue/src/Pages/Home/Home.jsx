d261f56706e3d3a0c9da35a95fcafb0afb71e07d
anano303 <124179773+anano303@users.noreply.github.com>
2024-02-25 01:51:50 +0400

    homePage

--- blue/src/Pages/Home/Home.jsx
+++ blue/src/Pages/Home/Home.jsx
@@ -1,4 +1,20 @@
+import "./Home.css";
+
 const Home = () => {
-  return <div className="homePage">homepage</div>;
+  return (
+    <div className="homePage">
+      <div className="homeText">
+        <h1>
+          რა არის შენი ცხოვრების მთავარი
+          <span>ბლუპრინტი? </span>
+        </h1>
+        <p>
+          You plan your vision, you plan your purpose that's we call your life
+          blueprint
+        </p>
+      </div>
+      <button className="homeButton"> აღმოაჩინე </button>
+    </div>
+  );
 };
 export default Home;
