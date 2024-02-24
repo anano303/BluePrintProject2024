66e04f72fd8f21ddb826552bbc2efcbb2365d3d0
anano303 <124179773+anano303@users.noreply.github.com>
2024-02-24 23:52:49 +0400

    footer text decoration

--- blue/src/Layouts/Layout.jsx
+++ blue/src/Layouts/Layout.jsx
@@ -1,10 +1,9 @@
 import Footer from "../Components/Footer/Footer";
-import Navbar from "../Components/navbar/navbar";
-import Header from "../Components/header/Header";
+import Navbar from "../Components/Navbar/Navbar";
+import Header from "../Components/Header/Header";
 export default function Layout({ children }) {
   return (
     <div>
-
       <Navbar />
       <Header />
 
