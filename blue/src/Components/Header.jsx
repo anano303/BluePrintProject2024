3d0ef92d115bc4032fac59348331ebbdd268126c
anano303 <124179773+anano303@users.noreply.github.com>
2024-02-24 21:46:48 +0400

    header menu

--- blue/src/Components/Header.jsx
+++ blue/src/Components/Header.jsx
@@ -1,4 +1,20 @@
+import logo from "../Assets/logo anim.png";
+import toggle from "../Assets/Icons/Toogle.png";
+import menu from "../Assets/Icons/Menu.png";
+import "./Header.css";
+
 const Header = () => {
-  return <div className="headerPage">Header page</div>;
+  return (
+    <div className="headerPage">
+      <div>
+        {" "}
+        <img src={logo} alt="logo" className="logo" />
+      </div>
+      <div className="icons">
+        <img src={toggle} alt="toggle" className="toggle" />
+        <img src={menu} alt="menu" className="menu" />
+      </div>
+    </div>
+  );
 };
 export default Header;
