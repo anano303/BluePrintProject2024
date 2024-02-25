99dd0767b7dbafe9b240d2038f2195f26be84049
teokartvelishvili <116433044+teokartvelishvili@users.noreply.github.com>
2024-02-25 05:12:42 -0800

    some translate

--- src/Languages.js
+++ src/Languages.js
@@ -1,18 +1,34 @@
 export const TEXTS = {
     ge: {
+        
         home: "სახლი",
         contact: "კონტაქტი",
         about: "ჩვენ შესახებ",
         services: "სერვისები",
         portfolio: "პორტფოლიო",
+        hwoAreWe: " ვინ ვართ ჩვენ? - მარკეტერები, რომლებიც საქმის სუპერ პროფესიონალები ვართ.",
+        ourVisionIs: "ჩვენი ხედვა ესეთია:",
+        provideCustomers: " მიაწოდე მომხმარებელს სიახლე, კრეატივი, ხარისხი და ემოცია. ",
+        whatIsYours: "რა არის შენი ",
+        blueprint: "ბლუპრინტი",
+        youPlanYourVision: "თქვენ გეგმავთ თქვენს ხედვას, თქვენ გეგმავთ თქვენს მიზანს, რასაც ჩვენ ვუწოდებთ თქვენს ცხოვრების გეგმას",
+        findOut: "აღმოაჩინე ",
     },
 
     en: {
+        
         home: "Home",
         contact: "Contact",
         about: "About",
         services: "Services",
         portfolio: "Portfolio",
+        hwoAreWe: "who are we? - Marketers who are super professionals.",
+        ourVisionIs: "Our vision is:",
+        provideCustomers: "Provide customers with news, creative, quality and emotion.",
+        whatIsYours: "what is yours",
+        blueprint: "blueprint",
+        youPlanYourVision: "You plan your vision, you plan your purpose that's we call your life blueprint",
+        findOut: "Find out",
 
     }
 }
