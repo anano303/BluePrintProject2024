c0b447efe173a274f879c0704ffdb21906ebc1aa
anano303 <124179773+anano303@users.noreply.github.com>
2024-02-25 13:54:03 +0400

    about page and header logo changes

--- blue/src/Pages/About/About.jsx
+++ blue/src/Pages/About/About.jsx
@@ -1,4 +1,13 @@
+import "./About.css";
 const About = () => {
-  return <div className="aboutPage">About page</div>;
+  return (
+    <div className="aboutPage">
+      <h3>
+        ვინ ვართ ჩვენ? - მარკეტერები, რომლებიც საქმის სუპერ პროფესიონალები ვართ.
+        <span> ჩვენი ხედვა ესეთია: </span>მიაწოდე მომხმარებელს სიახლე, კრეატივი,
+        ხარისხი და ემოცია.
+      </h3>
+    </div>
+  );
 };
 export default About;
