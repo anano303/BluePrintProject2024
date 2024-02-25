9ae7c010b2d286327f7372c90b3648ebafa36b3f
anano303 <124179773+anano303@users.noreply.github.com>
2024-02-25 14:58:54 +0400

    change uppercases

--- blue/src/Components/header/Header.jsx
+++ blue/src/Components/header/Header.jsx
@@ -1,17 +1,14 @@
-
-import React from 'react';
-import Navbar from '../navbar/navbar'; 
-import logo from '../Icons/logo anim.png';
-import './Header.css';
-import { ThemeContext } from '../../ThemeContext.js'
+import React from "react";
+import Navbar from "../navbar/navbar.jsx";
+import logo from "../Icons/logo anim.png";
+import "./Header.css";
+import { ThemeContext } from "../../ThemeContext.js";
 import { LanguageContext } from "../../LanguageContext.js";
-import { TEXTS } from "../../Languages.js"
+import { TEXTS } from "../../Languages.js";
 import { useContext } from "react";
 
-
-
 const Header = () => {
-  const  {language} = useContext(LanguageContext);
+  const { language } = useContext(LanguageContext);
 
   const themeContext = useContext(ThemeContext);
   const langContext = useContext(LanguageContext);
@@ -21,61 +18,67 @@ const Header = () => {
   };
   const darkOnClick = () => {
     themeContext.setTheme("dark");
-  }; 
-  const geoOnClick  = () => {
-      langContext.setLanguage("ge");
   };
-  const enOnClick  = () => {
-      langContext.setLanguage("en");   
+  const geoOnClick = () => {
+    langContext.setLanguage("ge");
+  };
+  const enOnClick = () => {
+    langContext.setLanguage("en");
   };
 
   return (
-
     <div className="headerPage">
       <div>
         <img src={logo} alt="logo" className="logo" />
       </div>
       <Navbar />
-      
-      <div>
-      <div className="contIcons">
-        <div className="ThemeMainDiv">
-          <div
-            className={
-              "lightMode " + (themeContext.theme === "light" ? "unvisible" : "")
-            }
-            onClick={ligthOnClick}
-          > 
-          <div className='darkLightStyles'></div>
-          </div> 
 
-          <div
-            className={
-              "darkMode " + (themeContext.theme === "dark" ? "unvisible" : "")
-            }
-            onClick={darkOnClick}
-          >
-            <div className='darkLightStyles'></div>
+      <div>
+        <div className="contIcons">
+          <div className="ThemeMainDiv">
+            <div
+              className={
+                "lightMode " +
+                (themeContext.theme === "light" ? "unvisible" : "")
+              }
+              onClick={ligthOnClick}
+            >
+              <div className="darkLightStyles"></div>
+            </div>
+
+            <div
+              className={
+                "darkMode " + (themeContext.theme === "dark" ? "unvisible" : "")
+              }
+              onClick={darkOnClick}
+            >
+              <div className="darkLightStyles"></div>
+            </div>
+          </div>
+
+          <div className="MainLangDiv">
+            <div
+              className={
+                "GeoFlag " + (langContext.language === "ge" ? "unvisible" : "")
+              }
+              onClick={geoOnClick}
+            >
+              <div className="darkLightStyles"></div>
+            </div>
+
+            <div
+              className={
+                "EnFlag " + (langContext.language === "en" ? "unvisible" : "")
+              }
+              onClick={enOnClick}
+            >
+              <div className="darkLightStyles"></div>{" "}
+            </div>
           </div>
         </div>
-
-        <div className="MainLangDiv">
-            <div 
-                className={"GeoFlag " + (langContext.language === "ge" ? "unvisible" : "")}
-                onClick={geoOnClick}>
-                 <div className='darkLightStyles'></div>
-            </div>
-            
-
-            <div 
-            className={"EnFlag " + (langContext.language === "en" ? "unvisible" : "")}
-            onClick={enOnClick} >
-               <div className='darkLightStyles'></div> </div>
-          </div>
       </div>
-      </div>
-      </div>
-  )
-}
+    </div>
+  );
+};
 
 export default Header;
--- blue/src/Components/navbar/navbar.jsx
+++ blue/src/Components/navbar/navbar.jsx
@@ -68,19 +68,19 @@
 
 // export default Navbar;
 // Navbar.jsx
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import './navbar.css';
-import closeImage from '../Icons/X.png';
-import toggleImage from '../Icons/Menu.png';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./navbar.css";
+import closeImage from "../Icons/X.png";
+import toggleImage from "../Icons/Menu.png";
 import { useContext } from "react";
 import { TEXTS } from "../../Languages.js";
-import { ThemeContext } from "../../ThemeContext";
-import { LanguageContext } from "../../LanguageContext";
+import { ThemeContext } from "../../ThemeContext.js";
+import { LanguageContext } from "../../LanguageContext.js";
 const Navbar = () => {
   const [showLinks, setShowLinks] = useState(false);
 
-  const  {language} = useContext(LanguageContext);
+  const { language } = useContext(LanguageContext);
 
   const themeContext = useContext(ThemeContext);
   const langContext = useContext(LanguageContext);
@@ -91,34 +91,32 @@ const Navbar = () => {
 
   return (
     <div>
+      <div className={`navbar ${showLinks ? "" : "hidden"}`}>
+        {showLinks && (
+          <ul className="links">
+            <li>
+              <Link to="/"> {TEXTS[language].home} </Link>
+            </li>
+            <li>
+              <Link to="/contact">{TEXTS[language].contact}</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/services">Services</Link>
+            </li>
+            <li>
+              <Link to="/portfolio">Portfolio</Link>
+            </li>
+          </ul>
+        )}
+      </div>
 
-    <div className={`navbar ${showLinks ? '' : 'hidden'}`}>
-      
-      {showLinks && (
-        <ul className="links">
-          <li>
-            <Link to="/"> {TEXTS[language].home} </Link>
-          </li>
-          <li>
-            <Link to="/contact">{TEXTS[language].contact}</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/services">Services</Link>
-          </li>
-          <li>
-            <Link to="/portfolio">Portfolio</Link>
-          </li>
-        </ul>
-      )}
- </div>
- 
-<div className="toggle-button" onClick={toggleLinks}>
-  <img src={showLinks ? closeImage : toggleImage} alt="Toggle" /> {/* Use appropriate images */}
-</div>
-   
+      <div className="toggle-button" onClick={toggleLinks}>
+        <img src={showLinks ? closeImage : toggleImage} alt="Toggle" />{" "}
+        {/* Use appropriate images */}
+      </div>
     </div>
   );
 };
