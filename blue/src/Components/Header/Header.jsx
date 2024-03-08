bf0a1955d86250ce39bc652d5ac7f685b8a49643
anano303 <124179773+anano303@users.noreply.github.com>
2024-03-08 16:07:43 +0400

    ./

---
+++ blue/src/Components/Header/Header.jsx
@@ -0,0 +1,84 @@
+import React from "react";
+import Navbar from "../Navbar/Navbar.jsx";
+import logo from "../Icons/logo anim.png";
+import "./Header.css";
+import { ThemeContext } from "../../ThemeContext.js";
+import { LanguageContext } from "../../LanguageContext.js";
+import { TEXTS } from "../../Languages.js";
+import { useContext } from "react";
+
+const Header = () => {
+  const { language } = useContext(LanguageContext);
+
+  const themeContext = useContext(ThemeContext);
+  const langContext = useContext(LanguageContext);
+
+  const ligthOnClick = () => {
+    themeContext.setTheme("light");
+  };
+  const darkOnClick = () => {
+    themeContext.setTheme("dark");
+  };
+  const geoOnClick = () => {
+    langContext.setLanguage("ge");
+  };
+  const enOnClick = () => {
+    langContext.setLanguage("en");
+  };
+
+  return (
+    <div className="headerPage">
+      <div>
+        <img src={logo} alt="logo" className="logo" />
+      </div>
+      <Navbar />
+
+      <div>
+        <div className="contIcons">
+          <div className="ThemeMainDiv">
+            <div
+              className={
+                "lightMode " +
+                (themeContext.theme === "light" ? "unvisible" : "")
+              }
+              onClick={ligthOnClick}
+            >
+              <div className="darkLightStyles"></div>
+            </div>
+
+            <div
+              className={
+                "darkMode " + (themeContext.theme === "dark" ? "unvisible" : "")
+              }
+              onClick={darkOnClick}
+            >
+              <div className="darkLightStyles"></div>
+            </div>
+          </div>
+
+          <div className="MainLangDiv">
+            <div
+              className={
+                "GeoFlag " + (langContext.language === "ge" ? "unvisible" : "")
+              }
+              onClick={geoOnClick}
+            >
+              <div className="darkLightStyles"></div>
+            </div>
+
+            <div
+              className={
+                "EnFlag " + (langContext.language === "en" ? "unvisible" : "")
+              }
+              onClick={enOnClick}
+            >
+              <div className="darkLightStyles"></div>{" "}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
