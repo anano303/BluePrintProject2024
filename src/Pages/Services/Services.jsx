1498c2bdb4cbb1f74d94bda337ad00de67e9c018
anano303 <124179773+anano303@users.noreply.github.com>
2024-05-02 02:34:43 +0400

    edit service page

--- src/Pages/Services/Services.jsx
+++ src/Pages/Services/Services.jsx
@@ -1,123 +1,81 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./Services.css";
-import blackArrowBUtton from "../Assets/black_arrow_button.png";
 import { TEXTS } from "../../Languages.js";
 import { LanguageContext } from "../../LanguageContext.js";
-import { useContext } from "react";
+import blackArrowBUtton from "../Assets/black_arrow_button.png";
+import GiftImg from "../Assets/სასაჩუქრე.WebP";
+import ProductionImg from "../Assets/პროდაქშენი.WebP";
+import SocialNetImg from "../Assets/სოციალური.WebP";
+import PolygraphImg from "../Assets/პოლიგრაფია.WebP";
+
+const servicesData = [
+  {
+    id: "01",
+    titleKey: "gift",
+    description:
+      "Unleash creativity with our curated gift ideas collection. Unique and personalized gift recommendations.",
+    imageUrl: GiftImg,
+  },
+  {
+    id: "02",
+    titleKey: "production",
+    description:
+      "Bringing visions to life through production expertise. Creative production solutions.",
+    imageUrl: ProductionImg,
+  },
+  {
+    id: "03",
+    titleKey: "socNet",
+    description:
+      "Navigating the digital landscape with effective social media management. Social media strategy and management.",
+    imageUrl: SocialNetImg,
+  },
+  {
+    id: "04",
+    titleKey: "polygraph",
+    description:
+      "Crafting brand identities that speak volumes. Unique and impactful brand identity designs.",
+    imageUrl: PolygraphImg,
+  },
+];
 
 const Services = () => {
   const { language } = useContext(LanguageContext);
+
   return (
-    <div className="servicesPage " id="services">
+    <div className="servicesPage" id="services">
       <h1>
         {TEXTS[language].studio} <br />
         <span>{TEXTS[language].services}</span>
       </h1>
       <div className="gridItems">
-        <div className="grid-item">
-          <div className="cut_left_corner project01 img">
-            <h1 className="transparentText ForProject">01</h1>
-            <div className="projectOnHover">
-              <div className="fullYellow"></div>
-              <button type="button">
-                <img id="black_arrow" src={blackArrowBUtton} alt="arrow" />
-              </button>
-            </div>{" "}
-          </div>
-          <div className="aboutProject">
-            <h3 className="projectCapture">{TEXTS[language].gift}</h3>
-            <p className="projectParagraph">
-              {" "}
-              Unleash creativity with our curated gift ideas collection.
-            </p>
-            <p className="grayText">
-              Unique and personalized gift recommendations.
-            </p>
-            <ul className="projectDotText">
-              <a href="mailto:Hello@blueprintstudio.ge">
-                <li> {TEXTS[language].order}</li>
-              </a>
-            </ul>
-          </div>
-        </div>
-
-        <div className="grid-item">
-          <div className="cut_left_corner project02 img">
-            <h1 className="transparentText ForProject">02</h1>
-            <div className="projectOnHover">
-              <div className="fullYellow"></div>
-              <button type="button">
-                <img id="black_arrow" src={blackArrowBUtton} alt="arrow" />
-              </button>
+        {servicesData.map((service) => (
+          <div className="grid-item" key={service.id}>
+            <div
+              className={`cut_left_corner img  project${service.id}`}
+              style={{ backgroundImage: `url(${service.imageUrl})` }}
+            >
+              <h1 className="transparentText ForProject">{service.id}</h1>
+              <div className="projectOnHover">
+                <div className="fullYellow"></div>
+                <button type="button">
+                  <img id="black_arrow" src={blackArrowBUtton} alt="arrow" />
+                </button>
+              </div>{" "}
             </div>
-          </div>
-          <div className="aboutProject">
-            <h3 className="projectCapture">{TEXTS[language].production}</h3>
-            <p className="projectParagraph">
-              Bringing visions to life through production expertise.
-            </p>
-            <p className="grayText">Creative production solutions.</p>
-            <ul className="projectDotText">
-              <a href="mailto:Hello@blueprintstudio.ge">
-                <li> {TEXTS[language].order}</li>
-              </a>
-            </ul>
-          </div>
-        </div>
-
-        <div className="grid-item">
-          <div className="cut_left_corner project03 img">
-            <h1 className="transparentText ForProject">03</h1>
-            <div className="projectOnHover">
-              <div className="fullYellow"></div>
-              <button type="button">
-                <img id="black_arrow" src={blackArrowBUtton} alt="arrow" />
-              </button>
-            </div>
-          </div>
-          <div className="aboutProject">
-            <h3 className="projectCapture">{TEXTS[language].socNet}</h3>
-            <p className="projectParagraph">
-              Navigating the digital landscape with effective social media
-              management.
-            </p>
-            <p className="grayText">Social media strategy and management.</p>
-            <ul className="projectDotText">
-              <a href="mailto:Hello@blueprintstudio.ge">
-                <li> {TEXTS[language].order}</li>
-              </a>
-            </ul>
-          </div>
-        </div>
-
-        <div className="grid-item">
-          <div className="cut_left_corner project04 img">
-            <h1 className="transparentText ForProject">04</h1>
-            <div className="projectOnHover">
-              <div className="fullYellow"></div>
-              <button type="button">
-                <img id="black_arrow" src={blackArrowBUtton} alt="arrow" />
-              </button>
-            </div>{" "}
-          </div>
-
-          <div className="aboutProject">
-            <div>
-              <h3 className="projectCapture">{TEXTS[language].polygraph}</h3>
-              <p className="projectParagraph">
-                Crafting brand identities that speak volumes.
-              </p>
-              <p className="grayText">
-                Unique and impactful brand identity designs.
-              </p>
+            <div className="aboutProject">
+              <h3 className="projectCapture">
+                {TEXTS[language][service.titleKey]}
+              </h3>
+              <p className="projectParagraph">{service.description}</p>
               <ul className="projectDotText">
                 <a href="mailto:Hello@blueprintstudio.ge">
-                  <li> {TEXTS[language].order}</li>
+                  <li>{TEXTS[language].order}</li>
                 </a>
               </ul>
             </div>
           </div>
-        </div>
+        ))}
       </div>
     </div>
   );
